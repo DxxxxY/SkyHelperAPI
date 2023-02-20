06270a333419918c01ca222f4694916ed8bd876e
dxxxxy <introsdream@gmail.com>
2023-02-20 09:14:37 -0500

    feat: removed auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -17,22 +17,25 @@ if (process.env.AUTH_TOKEN_URL) {
 
 //CREDIT: https://github.com/Senither/hypixel-skyblock-facade (Modified)
 module.exports = (req, res, next) => {
-    const AuthTokens = urlAuthTokens || process.env?.TOKENS?.split(',') || [];
+    // const AuthTokens = urlAuthTokens || process.env?.TOKENS?.split(',') || [];
 
-    if (req.headers.hasOwnProperty('authorization') && AuthTokens.includes(req.headers.authorization)) {
-        req.authToken = req.headers.authorization;
+    // if (req.headers.hasOwnProperty('authorization') && AuthTokens.includes(req.headers.authorization)) {
+    //     req.authToken = req.headers.authorization;
 
-        return next();
-    }
+    //     return next();
+    // }
 
-    if (req.query.hasOwnProperty('key') && AuthTokens.includes(req.query.key?.toString())) {
-        req.authToken = req.query.key?.toString();
+    // if (req.query.hasOwnProperty('key') && AuthTokens.includes(req.query.key?.toString())) {
+    //     req.authToken = req.query.key?.toString();
 
-        return next();
-    }
+    //     return next();
+    // }
 
-    return res.status(400).json({
-        status: 400,
-        reason: 'Missing "key" query parameter, or an "authorization" header with a valid SkyHelper API key',
-    });
+    // return res.status(400).json({
+    //     status: 400,
+    //     reason: 'Missing "key" query parameter, or an "authorization" header with a valid SkyHelper API key',
+    // });
+
+    //no keys
+    return next();
 };
